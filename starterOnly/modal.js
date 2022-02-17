4f829bee8108e30626d34da249643c7777aa2d97
ZaianeSchmitt <zaiane.schmitt@gmail.com>
2022-02-17 17:55:29 +0100

    Add event close modal after submit

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -12,37 +12,41 @@ const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
 const modalCross = document.querySelector(".close");
+const modalCloseBtn = document.querySelector(".btn-close");
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
-
-// close modal event
-modalCross.addEventListener("click", closeModal);
 
 // launch modal form
 function launchModal() {
   modalbg.style.display = "block";
 }
 
+// close modal event
+modalCross.addEventListener("click", closeModal); 
+  // close modal event after submission
+  modalCloseBtn.addEventListener("click", closeModal);
+
 // close modal form
 function closeModal() {
   modalbg.style.display = "none";
 }
 
-// Error messages
-  // Creation + naming
-  for (let formIt of formData){
-    const errText = document.createElement("p");
-    errText.className = "wrong-input";
-    formIt.appendChild(errText);
-  }
+// form validation
+  // error messages
+    // creation + naming
+    for (let formIt of formData){
+      const errText = document.createElement("p");
+      errText.className = "wrong-input";
+      formIt.appendChild(errText);
+    }
 
-  // Styling
-  const wrongInput = document.querySelectorAll(".wrong-input");
-  for (let errTextStyle of wrongInput){
-    errTextStyle.style.display = "none";
-    errTextStyle.style.color = "#ff4e60";
-    errTextStyle.style.fontSize = "12px";
-  }
+    // styling
+    const wrongInput = document.querySelectorAll(".wrong-input");
+    for (let errTextStyle of wrongInput){
+      errTextStyle.style.display = "none";
+      errTextStyle.style.color = "#ff4e60";
+      errTextStyle.style.fontSize = "12px";
+    }
 
-
+  
